dacb2d09125bfbd9ca891cfd9c3da1ec7f8f5182
Tobias Marczewski (mtoboid) <vortex@e.mail.de>
2022-03-08 16:46:57 +0100

    Added script to generate index for GithubPages.

---
+++ make-index.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Create an index for the ppa
+#
+# Show structure with tree
+# Remove links from directories and files that are no packages
+
+set -o errexit
+set -o nounset
+
+GITHUB_PAGES_ROOT='https://mtoboid.github.io/ppa'
+
+# generate index
+#
+tree -H "${GITHUB_PAGES_ROOT}" -T "PPA mtoboid" -I "*.html|_*|*.sh" --prune --noreport --charset utf-8 -o index.html
+
+
+# remove links
+#
+# 1) mask .deb packages
+sed -E -i 's/<a href=(".+\.deb")>(.*)<\/a>/<@@=\1@@\2>/g' index.html
+
+# 2) remove links
+sed -E -i 's/<a href=(".+")>(.*)<\/a>/\2/g' index.html
+
+# 3) re-instate .deb package links
+sed -E -i 's/<@@=(.*)@@([^>]*)>/<a href=\1>\2<\/a>/g' index.html
