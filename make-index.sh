982c9889c142206d84546732b49ff4bf4aa32e55
Tobias Marczewski (mtoboid) <mtoboid@e.mail.de>
2022-03-29 13:03:38 +0200

    Updated make-index.sh to run correctly when called from elsewhere.

--- make-index.sh
+++ make-index.sh
@@ -8,20 +8,28 @@
 set -o errexit
 set -o nounset
 
+SCRIPT_DIR=$( cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 GITHUB_PAGES_ROOT='https://mtoboid.github.io/ppa'
 
 # generate index
 #
-tree -H "${GITHUB_PAGES_ROOT}" -T "PPA mtoboid" -I "*.html|_*|*.sh" --prune --noreport --charset utf-8 -o index.html
+main() {
+    cd "${SCRIPT_DIR}"
+    tree -H "${GITHUB_PAGES_ROOT}" -T "PPA mtoboid" -I "*.html|_*|*.sh" --prune --noreport --charset utf-8 -o index.html
 
+    # remove links
+    #
+    # 1) mask .deb packages
+    sed -E -i 's/<a href=(".+\.deb")>(.*)<\/a>/<@@=\1@@\2>/g' index.html
 
-# remove links
-#
-# 1) mask .deb packages
-sed -E -i 's/<a href=(".+\.deb")>(.*)<\/a>/<@@=\1@@\2>/g' index.html
+    # 2) remove links
+    sed -E -i 's/<a href=(".+")>(.*)<\/a>/\2/g' index.html
 
-# 2) remove links
-sed -E -i 's/<a href=(".+")>(.*)<\/a>/\2/g' index.html
+    # 3) re-instate .deb package links
+    sed -E -i 's/<@@=(.*)@@([^>]*)>/<a href=\1>\2<\/a>/g' index.html
 
-# 3) re-instate .deb package links
-sed -E -i 's/<@@=(.*)@@([^>]*)>/<a href=\1>\2<\/a>/g' index.html
+    echo "Created new index at $(pwd)/index.html."
+    exit 0
+}
+
+main "$@"
